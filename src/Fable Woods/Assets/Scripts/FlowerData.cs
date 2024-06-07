bc3f357b5ea001d482fc249f2f94cfff40cd5426
Vinícius Gabriel <vinicius_gabriel258@hotmail.com>
2024-06-07 00:05:23 -0300

    Implement icon generation system

--- src/Fable Woods/Assets/Scripts/FlowerData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "FlowerData", menuName = "ScriptableObjects/FlowerData", order = 1)]
-public class FlowerData : ScriptableObject
-{
-    public int selectedPrefabIndex;
-
-    public GameObject[] flowerPrefabs;
-}
